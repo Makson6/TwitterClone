16a45a38c17a9be531321331f06143e5768b4659
CM <143717996+Makson6@users.noreply.github.com>
2024-04-15 12:23:47 +0300

    deleting data.jsx

--- src/composants/TweetEditor/TweetEditorForm/tweet-editor-input.jsx
+++ src/composants/TweetEditor/TweetEditorForm/tweet-editor-input.jsx
@@ -1,18 +1,20 @@
 import { useContext, useState } from "react";
+import TweetContent from "../../Tweets/TweetContent/TweetContent";
 
 
 export default function TweetEditorInput (){ 
   const [inputText,setInputText]=useState('');
-  const []=useContext()
+  
+  const [newTweet,setNewTweet]=useContext(TweetContent);
+
   const handleChange = (e)=> {
     setInputText(e.target.value)
     if(e.target.value.length == 100){
       alert("vous ne pouvez pas poster plus de 100 caracteres!!")
     }
     
-    
         
   };
   
-  return <input value={inputText} onChange={handleChange } maxLength={100} placeholder="What's happening?" className="tweet-editor-input" />
+  return <input value={newTweet} onChange={handleChange } maxLength={100} placeholder="What's happening?" className="tweet-editor-input" />
 }
